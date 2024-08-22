deb5e8d4795b19d00120c148d7a839dae8646969
unknown <simonhcui@gmail.com>
2024-08-22 12:06:02 -0700

    dmr

--- app.js
+++ app.js
@@ -6,25 +6,54 @@ const port = process.env.PORT || 3000;
 app.set("views", path.join(__dirname, "views"));
 
 // Read JSON file
-const rawData1 = fs.readFileSync(path.join(__dirname, "data/season1data.json"));
+const rawDataLifetime = fs.readFileSync(
+  path.join(__dirname, "data/lifetimedata.json")
+);
+const jsonDataLifetime = JSON.parse(rawDataLifetime);
+
+const rawDataDmr = fs.readFileSync(
+  path.join(__dirname, "data/dmr/dmrdata.json")
+);
+const jsonDataDmr = JSON.parse(rawDataDmr);
+
+const rawDataDmrColor = fs.readFileSync(
+  path.join(__dirname, "data/dmr/dmrcolordata.json")
+);
+const jsonDataDmrColor = JSON.parse(rawDataDmrColor);
+
+const rawData1 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season1data.json")
+);
 const jsonData1 = JSON.parse(rawData1);
 
-const rawData2 = fs.readFileSync(path.join(__dirname, "data/season2data.json"));
+const rawData2 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season2data.json")
+);
 const jsonData2 = JSON.parse(rawData2);
 
-const rawData3 = fs.readFileSync(path.join(__dirname, "data/season3data.json"));
+const rawData3 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season3data.json")
+);
 const jsonData3 = JSON.parse(rawData3);
 
-const rawData4 = fs.readFileSync(path.join(__dirname, "data/season4data.json"));
+const rawData4 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season4data.json")
+);
 const jsonData4 = JSON.parse(rawData4);
 
-const rawData5 = fs.readFileSync(path.join(__dirname, "data/season5data.json"));
+const rawData5 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season5data.json")
+);
 const jsonData5 = JSON.parse(rawData5);
 
-const rawData6 = fs.readFileSync(path.join(__dirname, "data/season6data.json"));
+const rawData6 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season6data.json")
+);
 const jsonData6 = JSON.parse(rawData6);
 
-const rawData7 = fs.readFileSync(path.join(__dirname, "data/season7data.json"));
+const rawData7 = fs.readFileSync(
+  path.join(__dirname, "data/past_seasons/season7data.json")
+);
 const jsonData7 = JSON.parse(rawData7);
 
 const rawData = fs.readFileSync(path.join(__dirname, "data/data.json"));
@@ -38,6 +67,23 @@ app.get("/", (req, res) => {
   res.render("home", {
     title: "Home",
     tableData: jsonData.tableData,
+  });
+});
+
+app.get("/lifetime", (req, res) => {
+  res.render("lifetime", {
+    title: "Lifetime",
+    id: req.params.id,
+    tableData: jsonDataLifetime.tableData,
+  });
+});
+
+app.get("/dmr", (req, res) => {
+  res.render("dmr", {
+    title: "DMR",
+    id: req.params.id,
+    tableData: jsonDataDmr.tableData,
+    colorData: jsonDataDmrColor.tableData,
   });
 });
 
